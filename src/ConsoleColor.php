73c7b11da2ed0f4f3f4bb74c6ca55b9827d181bc
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-03-25 12:39:42 +0200

    code readability

--- src/ConsoleColor.php
+++ src/ConsoleColor.php
@@ -206,13 +206,13 @@ class ConsoleColor
         if (DIRECTORY_SEPARATOR === '\\') {
             if (\function_exists('sapi_windows_vt100_support') && @\sapi_windows_vt100_support(STDOUT)) {
                 return true;
-            } elseif (\getenv('ANSICON') !== false || \getenv('ConEmuANSI') === 'ON') {
+            }
+            if (\getenv('ANSICON') !== false || \getenv('ConEmuANSI') === 'ON') {
                 return true;
             }
             return false;
-        } else {
-            return \function_exists('posix_isatty') && @\posix_isatty(STDOUT);
         }
+        return \function_exists('posix_isatty') && @\posix_isatty(STDOUT);
     }
 
     /**
@@ -222,9 +222,9 @@ class ConsoleColor
     {
         if (DIRECTORY_SEPARATOR === '\\') {
             return \function_exists('sapi_windows_vt100_support') && @\sapi_windows_vt100_support(STDOUT);
-        } else {
-            return \strpos(\getenv('TERM'), '256color') !== false;
         }
+
+        return \strpos(\getenv('TERM'), '256color') !== false;
     }
 
     /**
